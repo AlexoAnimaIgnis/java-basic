b9b4a034051002c4bfd667ec5e589566fb319e78
unknown <ballester.alexander@gmail.com>
2020-07-26 15:13:36 +0800

    provided test class in preparation for stream operations

---
+++ src/test/com/java/exercises/streams/StreamOperationsTest.java
@@ -0,0 +1,47 @@
+package test.com.java.exercises.streams;
+
+
+import org.junit.Test;
+
+public class StreamOperationsTest {
+
+    @Test
+    public void operationForEach() {
+
+    }
+
+    @Test
+    public void operationMap() {
+
+    }
+
+    @Test
+    public void operationCollect() {
+
+    }
+
+    @Test
+    public void operationFilter() {
+
+    }
+
+    @Test
+    public void operationFindFirst() {
+
+    }
+
+    @Test
+    public void operationToArray() {
+
+    }
+
+    @Test
+    public void operationFlatMap() {
+
+    }
+
+    @Test
+    public void operationPeek() {
+
+    }
+}
