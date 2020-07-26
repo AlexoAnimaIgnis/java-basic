62194b4cd85ec8b990e77512029e5ed33150c6c3
unknown <ballester.alexander@gmail.com>
2020-07-26 15:49:37 +0800

    provided implementation for stream foreach

--- src/main/com/java/exercises/streams/Employee.java
+++ src/main/com/java/exercises/streams/Employee.java
@@ -3,11 +3,27 @@ package main.com.java.exercises.streams;
 public class Employee {
     private int id;
     private String name;
-    private double netWorth;
+    double salary;
 
-    public Employee(int id, String name, double netWorth) {
+    public Employee(int id, String name, double salary) {
         this.id = id;
         this.name = name;
-        this.netWorth = netWorth;
+        this.salary = salary;
+    }
+
+    public void salaryIncrement(double salary) {
+        this.salary += salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
     }
 }
--- src/test/com/java/exercises/streams/StreamOperationsTest.java
+++ src/test/com/java/exercises/streams/StreamOperationsTest.java
@@ -1,13 +1,34 @@
 package test.com.java.exercises.streams;
 
 
+import main.com.java.exercises.streams.Employee;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
 public class StreamOperationsTest {
 
+    private static Employee[] arrayOfEmps = {
+            new Employee(1, "Jeff Bezos", 100000.0),
+            new Employee(2, "Bill Gates", 200000.0),
+            new Employee(3, "Mark Zuckerberg", 300000.0)
+    };
+
+    private List<Employee> employeeList = Arrays.asList(arrayOfEmps);
+
     @Test
     public void operationForEach() {
+        employeeList.stream().forEach( e -> { e.salaryIncrement(10.0);});
 
+        assertThat(employeeList, contains(
+                hasProperty("salary", equalTo(100010.0)),
+                hasProperty("salary", equalTo(200010.0)),
+                hasProperty("salary", equalTo(300010.0))
+        ));
     }
 
     @Test
